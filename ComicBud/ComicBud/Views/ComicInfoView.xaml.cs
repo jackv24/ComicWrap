dd4676b594a9a9d3a3f49b04270b522718b4ebc0
Jack Vine <jackv949@gmail.com>
2020-02-03 11:45:34 +1030

    Fixed ComicInfoView to behave like a proper custom control, using bindings

--- ComicBud/ComicBud/Views/ComicInfoView.xaml.cs
+++ ComicBud/ComicBud/Views/ComicInfoView.xaml.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using FreshMvvm;
+
+using ComicBud.Systems;
+using ComicBud.Pages;
+
 namespace ComicBud.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,6 +20,39 @@ namespace ComicBud.Views
         public ComicInfoView()
         {
             InitializeComponent();
+
+            var tapGesture = new TapGestureRecognizer();
+            tapGesture.Command = new Command(async () => await OpenComic());
+            GestureRecognizers.Add(tapGesture);
+        }
+
+        public static BindableProperty ComicProperty = BindableProperty.Create(
+            propertyName: "Comic",
+            returnType: typeof(Comic),
+            declaringType: typeof(ComicInfoView),
+            defaultBindingMode: BindingMode.OneWay,
+            propertyChanged: OnComicNamePropertyChanged
+            );
+
+        public Comic Comic
+        {
+            get { return (Comic)GetValue(ComicProperty); }
+            set { SetValue(ComicProperty, value); }
+        }
+
+        private static void OnComicNamePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var comicInfoView = (ComicInfoView)bindable;
+            var comic = (Comic)newValue;
+
+            comicInfoView.labelComicName.Text = comic.ArchiveUrl;
+        }
+
+        private async Task OpenComic()
+        {
+            var navService = FreshIOC.Container.Resolve<IFreshNavigationService>(Constants.DefaultNavigationServiceName);
+            var page = FreshPageModelResolver.ResolvePageModel<ComicDetailPageModel>(Comic);
+            await navService.PushPage(page, null);
         }
     }
 }

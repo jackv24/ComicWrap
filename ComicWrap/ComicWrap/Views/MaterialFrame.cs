d679086bd9a7935eadea53e0293eb1ad8dc9c879
Jack Vine <jackv949@gmail.com>
2020-05-02 17:02:25 +0930

    ComicInfoView Frame shadows are now more consistent across Android and iOS
    Fixed ComicUpdateInfoView clipping on Android (didn't actually fix the clipping, just adjusted padding and margins)

---
+++ ComicWrap/ComicWrap/Views/MaterialFrame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ComicWrap.Views
+{
+    // Original source: https://alexdunn.org/2018/06/06/xamarin-tip-dynamic-elevation-frames/
+    public class MaterialFrame : Frame
+    {
+        public static BindableProperty ElevationProperty = BindableProperty.Create(
+            nameof(Elevation),
+            typeof(float),
+            typeof(MaterialFrame),
+            defaultValue: 4.0f);
+
+        public float Elevation
+        {
+            get { return (float)GetValue(ElevationProperty); }
+            set { SetValue(ElevationProperty, value); }
+        }
+    }
+}
